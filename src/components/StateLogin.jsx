bf028771382503dca36fb84811826a5a7e2ffb38
JadeRoss <105959739+Jetpack019@users.noreply.github.com>
2024-11-13 16:10:54 +0800

    validating input upon lost focus

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -7,7 +7,12 @@ export default function Login() {
     password: '',	
   });
 
-  const emailIsInvalid = enteredValues.email !== '' && !enteredValues.email.includes('@');
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  })
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
   
   function handleSubmit(event) {
     event.preventDefault();
@@ -18,6 +23,18 @@ export default function Login() {
     setEnteredValues(prevValues => ({
       ...prevValues,[identifier] : value
     }))
+
+    setDidEdit( prevEdit => ({
+      ...prevEdit,
+      [identifier] : false
+    }))
+  }
+
+  function handleInputBlur(identifier){
+    setDidEdit( prevEdit => ({
+      ...prevEdit,
+      [identifier] : true
+    }))
   }
 
   return (
@@ -27,7 +44,7 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" onChange={(event) => handleInputChange('email',event.target.value) } value={enteredValues.email}/>
+          <input id="email" type="email" name="email" onBlur={() => handleInputBlur('email')} onChange={(event) => handleInputChange('email',event.target.value) } value={enteredValues.email}/>
           <div className="control-error">
             {emailIsInvalid && <p>Please enter a valid email address</p> }
           </div>
