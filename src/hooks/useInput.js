a6f41d58733c2dee9df9b314833e96919215d285
JadeRoss <105959739+Jetpack019@users.noreply.github.com>
2025-05-11 09:19:21 +0800

    customHook

---
+++ src/hooks/useInput.js
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+function useInput(defaultValue,validationFn) {
+    const [enteredValue,setEnteredValues] = useState(defaultValue);
+    const [didEdit, setDidEdit] = useState(false)
+
+    const valueIsValid = validationFn(enteredValue)
+ 
+    function handleInputChange(event){
+        setEnteredValues(event.target.value)
+        setDidEdit(false)
+    }
+    
+    function handleInputBlur(){
+        setDidEdit(true)
+    }
+
+    return{
+        value: enteredValue,
+        handleInputChange,
+        handleInputBlur,
+        hasError: didEdit && !valueIsValid
+    };
+    
+}
+
+export default useInput
